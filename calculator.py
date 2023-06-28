e8a9ac7928877e0502b8f16ac12911bd31479d5d
Travis Cassell <71227973+travisgazelle@users.noreply.github.com>
2023-06-27 17:53:06 -0700

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def main():
+    pass
+
+
+def square(n):
+    return n * n
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_calculator.py
@@ -0,0 +1,25 @@
+from calculator import square
+
+
+def main():
+    test_positive()
+    test_negative()
+    test_zero()
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+
+def test_zero():
+    assert square(0) == 0
+
+
+if __name__ == '__main__':
+    main()
